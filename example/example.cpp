ad0b5d51a6b5b840ef38e92eb1cbd53c98ea23e5
Frederik Tobner <masterhorst90@web.de>
2023-03-05 16:09:59 +0100

    feat: asserts and compile definitions

--- example/example.cpp
+++ example/example.cpp
@@ -2,6 +2,7 @@
 #include "entry_point.hpp"
 #include "core/tobot_application.h"
 #include "utilities/logger.hpp"
+#include "utilities/tobot_asserts.hpp"
 
 class Example : public Tobot::Core::TobotApplication
 {
@@ -20,6 +21,7 @@ public:
 Tobot::Core::TobotApplication * Tobot::Core::CreateApplication()
 {
     Tobot::Utilities::Logger::EnableFileOutput();
-    LOG_INFO("Hello from Example");
+    LOG_INFO("Hello from Example %i", 123);
+    TOBOT_ASSERT(2 == 1 + 1)
     return new Example();
 }
