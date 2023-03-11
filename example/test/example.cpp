9b827fdc3ae4a4d00f7be6490cefbbc8f0add6ce
Frederik Tobner <masterhorst90@web.de>
2023-03-11 21:45:24 +0100

    feat: added more test marco`s
    added additional test macro's

--- example/test/example.cpp
+++ example/test/example.cpp
@@ -3,13 +3,14 @@
 TOBOT_TEST(example, first) {
     EXPECT_TRUE(false)
     ASSERT_TRUE(true)
+    ASSERT_STREQUAL("test", "test")
 }
 
 int main(int argc, char const ** argv) {
     int result = 0;
-    tobot_testexample_first(&result);
+    test_example_first(&result);
     if (result) {
-        std::cerr << "Test failed" << std::endl;
+        return -1;
     }
     return 0;
 }
--- packages/tobot_tooling/src/test/tobot_test.h
+++ packages/tobot_tooling/src/test/tobot_test.h
@@ -1,17 +1,42 @@
 #pragma once
 
 namespace Tobot::Tooling::Test {
-#define TOBOT_TEST(testsuiteName, testName, ...) void tobot_test##testsuiteName##_##testName##(int * result)
+#define TOBOT_TEST(testsuiteName, testName, ...) void test_##testsuiteName##_##testName##(int * result)
 
-#define ASSERT_TRUE(condition)                        \
-    if (!condition) {                                 \
-        std::cout << "Assertion failed" << std::endl; \
-        *result = -1;                                 \
-        return;                                       \
+#define ASSERT_TRUE(condition)                                                                   \
+    if (!condition) {                                                                            \
+        std::cout << "Assertion failed at line " << __LINE__ << " in " << __FILE__ << std::endl; \
+        *result = -1;                                                                            \
+        return;                                                                                  \
     }
-#define EXPECT_TRUE(condition)                    \
-    if (!condition) {                             \
-        std::cout << "Check failed" << std::endl; \
-        *result = -1;                             \
+
+#define EXPECT_TRUE(condition)                                                               \
+    if (!condition) {                                                                        \
+        std::cout << "Check failed at line " << __LINE__ << " in " << __FILE__ << std::endl; \
+        *result = -1;                                                                        \
     }
+
+#define ASSERT_EQUAL(a, b)                                                                       \
+    if (a != b) {                                                                                \
+        std::cout << "Assertion failed at line " << __LINE__ << " in " << __FILE__ << std::endl; \
+        *result = -1;                                                                            \
+        return;                                                                                  \
+    }
+#define EXPECT_EQUAL(a, b)                                                                   \
+    if (a != b) {                                                                            \
+        std::cout << "Check failed at line " << __LINE__ << " in " << __FILE__ << std::endl; \
+        *result = -1;                                                                        \
+    }
+#define ASSERT_STREQUAL(a, b)                                                                       \
+    if (strcmp(a, b)) {                                                                                \
+        std::cout << "Assertion failed at line " << __LINE__ << " in " << __FILE__ << std::endl; \
+        *result = -1;                                                                            \
+        return;                                                                                  \
+    }
+#define EXPECT_STREQUAL(a, b)                                                                   \
+    if (strcmp(a, b)) {                                                                            \
+        std::cout << "Check failed at line " << __LINE__ << " in " << __FILE__ << std::endl; \
+        *result = -1;                                                                        \
+    }
+
 } // namespace Tobot::Tooling::Test
