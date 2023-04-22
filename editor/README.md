0ad09689a64c9ef291d2ef950aff932ff53adf3f
Frederik Tobner <masterhorst90@web.de>
2023-04-22 18:38:40 +0200

    feat: :art: changed font and icon

---
+++ editor/README.md
@@ -0,0 +1,8 @@
+# Tobot-Editor
+
+The Tobot-Editor is a graphical editor for the Tobot-Engine.
+
+## Dependencies
+
+* [SDL2](https://www.libsdl.org/)
+* [Imgui](https://github.com/ocornut/imgui)
--- editor/main.cpp
+++ editor/main.cpp
@@ -1,17 +1,30 @@
+#include "SDL.h"
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_sdlrenderer.h"
-#include <SDL.h>
-#include <stdio.h>
 
+#include <stdio.h>
 
 #if !SDL_VERSION_ATLEAST(2, 0, 17)
 #error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
 #endif
 
+void handleEvents(SDL_Event & event, bool & done, SDL_Window * window) {
+    while (SDL_PollEvent(&event)) {
+        ImGui_ImplSDL2_ProcessEvent(&event);
+        if (event.type == SDL_QUIT) {
+            done = true;
+        }
+        if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE &&
+            event.window.windowID == SDL_GetWindowID(window)) {
+            done = true;
+        }
+    }
+}
+
 // Main code
-int main(int, char **) {
-    // Setup SDL
+int main(int argc, char ** argv) {
+    // Seting up SDL
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
         printf("Error: %s\n", SDL_GetError());
         return -1;
@@ -24,8 +37,16 @@ int main(int, char **) {
 
     // Create window with SDL_Renderer graphics context
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Window * window = SDL_CreateWindow("Dear ImGui SDL2+SDL_Renderer example", SDL_WINDOWPOS_CENTERED,
-                                           SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+    SDL_Window * window =
+        SDL_CreateWindow("Tobot-Editor", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+    SDL_Surface * window_icon_scurface = SDL_LoadBMP("./assets/icon.bmp");
+    if (!window_icon_scurface) {
+        printf("Failed to load window icon %s\n", SDL_GetError());
+        return -1;
+    }
+    // Setting the icon of the window
+    SDL_SetWindowIcon(window, window_icon_scurface);
+    SDL_FreeSurface(window_icon_scurface);
     SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
     if (renderer == nullptr) {
         SDL_Log("Error creating SDL_Renderer!");
@@ -44,8 +65,7 @@ int main(int, char **) {
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
 
     // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    // ImGui::StyleColorsLight();
+    ImGui::StyleColorsLight();
 
     // Setup Platform/Renderer backends
     ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
@@ -54,19 +74,21 @@ int main(int, char **) {
     // Load Fonts
     // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use
     // ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among
+    // multiple.
     // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your
     // application (e.g. use an assertion, or display an error and quit).
     // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling
     // ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
+    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font
+    // rendering.
     // - Read 'docs/FONTS.md' for more instructions and details.
     // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double
     // backslash \\ !
     // io.Fonts->AddFontDefault();
     // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
     // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    io.Fonts->AddFontFromFileTTF("./fonts/Roboto-Medium.ttf", 16.0f);
     // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
     // ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr,
     // io.Fonts->GetGlyphRangesJapanese()); IM_ASSERT(font != nullptr);
@@ -80,32 +102,23 @@ int main(int, char **) {
     bool done = false;
     while (!done) {
         // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your
-        // inputs.
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use
+        // your inputs.
         // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or
         // clear/overwrite your copy of the mouse data.
         // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or
-        // clear/overwrite your copy of the keyboard data. Generally you may always pass all inputs to dear imgui, and
-        // hide them from your application based on those two flags.
+        // clear/overwrite your copy of the keyboard data. Generally you may always pass all inputs to dear imgui,
+        // and hide them from your application based on those two flags.
         SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            ImGui_ImplSDL2_ProcessEvent(&event);
-            if (event.type == SDL_QUIT) {
-                done = true;
-            }
-            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE &&
-                event.window.windowID == SDL_GetWindowID(window)) {
-                done = true;
-            }
-        }
+        handleEvents(event, done, window);
 
         // Start the Dear ImGui frame
         ImGui_ImplSDLRenderer_NewFrame();
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code
-        // to learn more about Dear ImGui!).
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its
+        // code to learn more about Dear ImGui!).
         if (show_demo_window) {
             ImGui::ShowDemoWindow(&show_demo_window);
         }
@@ -124,8 +137,8 @@ int main(int, char **) {
             ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
             ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
 
-            if (ImGui::Button(
-                    "Button")) { // Buttons return true when clicked (most widgets return true when edited/activated)
+            if (ImGui::Button("Button")) { // Buttons return true when clicked (most widgets return true when
+                                           // edited/activated)
                 counter++;
             }
             ImGui::SameLine();
@@ -138,8 +151,8 @@ int main(int, char **) {
         // 3. Show another simple window.
         if (show_another_window) {
             ImGui::Begin("Another Window",
-                         &show_another_window); // Pass a pointer to our bool variable (the window will have a closing
-                                                // button that will clear the bool when clicked)
+                         &show_another_window); // Pass a pointer to our bool variable (the window will have a
+                                                // closing button that will clear the bool when clicked)
             ImGui::Text("Hello from another window!");
             if (ImGui::Button("Close Me")) {
                 show_another_window = false;
